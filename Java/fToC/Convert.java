d353d2183903ee314afb3ee4c0cccce7bc9b249a
Tibor Pelegrin <peletibgrinor@gmail.com>
2021-05-16 15:25:45 +0200

    update z domu

---
+++ Java/fToC/Convert.java
@@ -0,0 +1,15 @@
+/*
+Convert
+Tibor Pelegrin;
+3.A
+*/
+
+//trieda Convert
+public class Convert{
+  //staticka metoda na premenu stupnov fahrenheita na stupne celsius 
+  //metoda prebera ako parameter stupne fahrenheita
+  public static float fahrenheitToCelsius(float fahrenheit){
+    return (fahrenheit - 32F) / 1.8F; 
+  }
+}
+
